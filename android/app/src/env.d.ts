a1930e3b931cba51e8be755ed66206ed8a294b21
Yousif <yousifae8@gmail.com>
2026-07-17 16:55:24 +0300

    feat: implement Firebase authentication with environment variable configuration and user state management

---
+++ android/app/src/env.d.ts
@@ -0,0 +1,9 @@
+declare module '@env' {
+    export const GOOGLE_API_KEY: string;
+    export const GOOGLE_AUTH_DOMAIN: string;
+    export const GOOGLE_PROJECT_ID: string;
+    export const GOOGLE_STORAGE_BUCKET: string;
+    export const GOOGLE_MESSAGING_SENDER_ID: string;
+    export const GOOGLE_APP_ID: string;
+    export const GOOGLE_MEASUREMENT_ID: string;
+}
